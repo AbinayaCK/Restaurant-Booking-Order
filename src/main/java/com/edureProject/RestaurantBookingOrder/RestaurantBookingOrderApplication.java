bedefc6a19b2e56232bddecbea77f629a9370c9c
Akshaya c <cabinaya12@gmail.com>
2025-07-28 21:22:04 +0530

    Spring Starter initialized

---
+++ src/main/java/com/edureProject/RestaurantBookingOrder/RestaurantBookingOrderApplication.java
@@ -0,0 +1,13 @@
+package com.edureProject.RestaurantBookingOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookingOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBookingOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edureProject/RestaurantBookingOrder/RestaurantBookingOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureProject.RestaurantBookingOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBookingOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
